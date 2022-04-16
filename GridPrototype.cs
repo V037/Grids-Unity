524f95e7b244a90f92f23f0a1cb1d45d48de9a19
Valentino <69222741+V037@users.noreply.github.com>
2022-04-17 01:26:51 +0200

    Update GridPrototype.cs

--- GridPrototype.cs
+++ GridPrototype.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//credits: V.037_
+
 public class GridPrototype : MonoBehaviour
 {
     private bool buildModeOn = false;
