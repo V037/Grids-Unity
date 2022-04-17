bbbf26e6e5d4a4a9e7ab86c345ed0a15ddd22259
Valentino <69222741+V037@users.noreply.github.com>
2022-04-17 20:49:55 +0200

    Update Place_Blocks.cs

--- Place_Blocks.cs
+++ Place_Blocks.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+
+//credits: V037_
+
 public class GridPrototype : MonoBehaviour
 {
-  public vector3 V3;
-  Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-  RaycastHit hitInfo;
-  
-  if (Physics.Raycast(ray, out hitInfo))
+  private void MoveCurrentObjectToMouse()
   {
-    V3 = hitInfo.point + hitInfo.normal /2; //using raycast
-  }
-  
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      public vector3 V3;
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hitInfo;
+      
+      if (Physics.Raycast(ray, out hitInfo))
+      {
+        V3 = hitInfo.point + hitInfo.normal /2; //the important part using the power of the math
+      }
+   }
 }
